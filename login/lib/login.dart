9c5e3055ec0a6c158e44f6222ae8848fdbf681ff
taratalexander <taratalexander@gmail.com>
2022-09-15 22:19:25 +0400

    Refactoring the code.

--- login/lib/login.dart
+++ login/lib/login.dart
@@ -8,6 +8,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:gap/gap.dart';
 
-part 'src/login_page.dart';
+part 'src/login_screen.dart';
 
 part 'state/auth_state_notifier.dart';
