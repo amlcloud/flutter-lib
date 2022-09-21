afeaff55e87af94f100c4cc3c0ad21f1aa796daf
taratalexander <taratalexander@gmail.com>
2022-09-22 00:16:13 +0400

    Init commit

---
+++ userprofile/lib/userprofile.dart
@@ -0,0 +1,9 @@
+library userprofile;
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+part 'avatar_image.dart';
