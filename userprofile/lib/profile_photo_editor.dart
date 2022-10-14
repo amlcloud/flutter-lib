1f483e1ee6b05badba5d60af53075da4d41adb43
taratalexander <taratalexander@gmail.com>
2022-10-14 08:12:49 +0400

    Fixing UI issues

--- userprofile/lib/profile_photo_editor.dart
+++ userprofile/lib/profile_photo_editor.dart
@@ -94,40 +94,48 @@ class ProfilePhotoEditor extends ConsumerWidget {
       ]);
 
   Widget buildPhoto(BuildContext context, WidgetRef ref) => Center(
-      child: ref
-          .watch(docSP('userInfo/${FirebaseAuth.instance.currentUser!.uid}'))
-          .when(
+        child: ref
+            .watch(docSP('userInfo/${FirebaseAuth.instance.currentUser!.uid}'))
+            .when(
               loading: () => Container(),
               error: (e, s) => ErrorWidget(e),
-              // data: (userInfo) => CircleAvatar(
-              //   radius: 50,
-              //   backgroundImage: userInfo.exists &&
-              //           !(userInfo.data()?['photoUrl'] ?? '').isEmpty
-              //       ? Image.network(userInfo.data()!['photoUrl'],
-              //               width: 50, height: 50)
-              //           .image
-              //       : (FirebaseAuth.instance.currentUser?.photoURL?.isNotEmpty==true
-              //          ? Image.network(
-              //                   FirebaseAuth.instance.currentUser!.photoURL!)
-              //               .image):,
+              data: (userInfo) => CircleAvatar(
+                radius: 50,
+                backgroundImage: userInfo.exists &&
+                        !(userInfo.data()?['photoUrl'] ?? '').isEmpty
+                    ? Image.network(userInfo.data()!['photoUrl'],
+                            width: 50, height: 50)
+                        .image
+                    : (FirebaseAuth.instance.currentUser?.photoURL == null
+                        ? null
+                        : Image.network(
+                                FirebaseAuth.instance.currentUser!.photoURL!)
+                            .image),
+                child: Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: const [Icon(Icons.person)],
+                ),
+              ),
+            ),
+      );
+  // data: (userInfo) => Center(
+  //       child: userInfo.exists &&
+  //               !(userInfo.data()?['photoUrl'] ?? '').isEmpty
+  //           ? CircleAvatar(
+  //               backgroundImage: Image.network(
+  //                       userInfo.data()!['photoUrl'],
+  //                       width: 50,
+  //                       height: 50)
+  //                   .image)
+  //           : FirebaseAuth.instance.currentUser?.photoURL == null
+  //               ? const Icon(Icons.person)
+  //               : CircleAvatar(
+  //                   radius: 14,
+  //                   backgroundImage: Image.network(FirebaseAuth
+  //                           .instance.currentUser!.photoURL!)
+  //                       .image),
+  //     )));
 
-              data: (userInfo) => Center(
-                    child: userInfo.exists &&
-                            !(userInfo.data()?['photoUrl'] ?? '').isEmpty
-                        ? CircleAvatar(
-                            backgroundImage: Image.network(
-                                    userInfo.data()!['photoUrl'],
-                                    width: 50,
-                                    height: 50)
-                                .image)
-                        : FirebaseAuth.instance.currentUser?.photoURL == null
-                            ? const Icon(Icons.person)
-                            : CircleAvatar(
-                                radius: 14,
-                                backgroundImage: Image.network(FirebaseAuth
-                                        .instance.currentUser!.photoURL!)
-                                    .image),
-                  )));
   // ref.watch(docStreamProvider('org/$uid')).when(
   //     data: (org) => org.data() == null || org.data()!['logoUrl'] == null
   //         ? Center(
