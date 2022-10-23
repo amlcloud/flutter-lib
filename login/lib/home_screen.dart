65ea0a4b612a15743ca64394cd94ea31e912f59c
taratalexander <taratalexander@gmail.com>
2022-10-23 23:33:01 +0400

    Fixed the error

--- login/lib/home_screen.dart
+++ login/lib/home_screen.dart
@@ -4,7 +4,7 @@ Color transparent = const Color.fromARGB(255, 255, 255, 255);
 
 enum Option { login, signUp }
 
-final isLoginProvider = StateNotifierProvider((_) => BooleanNotifier(false));
+final isLoginProvider = StateNotifierProvider((_) => AuthStateNotifier(false));
 
 final loginState = Provider((ref) => ref.watch(isLoginProvider));
 
@@ -18,10 +18,10 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-      final notifier = ref.watch(isLoginProvider.notifier);
+    final notifier = ref.watch(isLoginProvider.notifier);
 
     var isLogin = ref.watch(loginState);
-    
+
     Option getUserSelection(bool isLogin) {
       if (isLogin) {
         return Option.login;
@@ -30,45 +30,44 @@ class HomePage extends ConsumerWidget {
       }
     }
 
-    return  Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: AnimatedSwitcher(
-                duration: const Duration(milliseconds: 500),
-                child: Container(
-                  child: getUserSelection(isLogin as bool) == Option.login
-                      ? LoginScreen(
-                          loginOptions: loginOptions,
-                          screenTitle: screenTitle,
-                          onSignUpSelected: () {
-                            notifier.value = false;
-                          },
-                        )
-                      : SignupScreen(
-                          onLogInSelected: () {
-                            notifier.value = true;
-                          },
-                        ),
-                )
-                //}
-                ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              alignment: Alignment.center,
-              decoration: const BoxDecoration(color: Colors.blueGrey),
-              child: const Text(
-                "AML",
-                style: TextStyle(
-                  fontSize: 25,
-                ),
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 500),
+              child: Container(
+                child: getUserSelection(isLogin as bool) == Option.login
+                    ? LoginScreen(
+                        loginOptions: loginOptions,
+                        screenTitle: screenTitle,
+                        onSignUpSelected: () {
+                          notifier.value = false;
+                        },
+                      )
+                    : SignupScreen(
+                        onLogInSelected: () {
+                          notifier.value = true;
+                        },
+                      ),
+              )
+              //}
+              ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            alignment: Alignment.center,
+            decoration: const BoxDecoration(color: Colors.blueGrey),
+            child: const Text(
+              "AML",
+              style: TextStyle(
+                fontSize: 25,
               ),
             ),
           ),
-        ],
-      ),
-  
+        ),
+      ],
+    );
   }
 }
